e6609694b7a90babaa29c28d709186501cc56b6d
user <user@DESKTOP-QBELQM8>
2026-01-07 20:53:34 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdZZiM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
